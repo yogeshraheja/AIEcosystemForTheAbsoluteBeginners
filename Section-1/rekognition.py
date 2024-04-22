a49416103b909d035180cb9bcaf3dea51f1975e0
naman-thinknyx <155966743+naman-thinknyx@users.noreply.github.com>
2024-04-22 13:07:08 +0530

    Add files via upload

---
+++ Section-1/rekognition.py
@@ -0,0 +1,19 @@
+import json 
+import boto3 #library which provides an interface to interact with various AWS services
+
+#creating a client object for the Rekognition service
+api = boto3.client('rekognition') 
+
+def lambda_handler(event, context):
+    #provide the name of your S3 bucket in the sbucket variable 
+    sbucket = "thinknyxdemo1"     
+
+    #name of the image in the image variable
+    image = "Diya.jpeg"  
+
+    #using the detect_faces method of the Rekognition client object
+    result = api.detect_faces(Image = {'S3Object':{'Bucket': sbucket, 'Name': image}}, Attributes=['ALL']) 
+
+    #iterating over the list of face details returned by Rekognition and print them in a json format
+    for face in result['FaceDetails']: 
+        print(json.dumps (face, indent = 2)) 
